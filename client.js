f97a07ea26c604d7ab76f82a364c4f2d62160ea3
opzmetek <op.zmetek@gmail.com>
2025-12-17 18:04:22 +0100

    Add image loading for debugging
    Added image loading for debugging purposes.

--- client.js
+++ client.js
@@ -1,6 +1,9 @@
 console.log("t2 loading");
 import * as T2 from "https://shipsio.pages.dev/t2.module.js";
 console.log("t2 loaded");
+const img = new Image();
+img.src="https://www.reddit.com/r/battlemaps/comments/ls5z3m/transparent_ships/";//debug
+img.crossOrigin="anonymous";
 const r=new T2.Renderer(document.getElementById("game"),new OffscreenCanvas(1600,800));
 await r.start();
 const ship=new T2.ORectangle(-0.5,-1,1,2);
