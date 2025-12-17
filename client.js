5ef95081b9297d4f51223ee1a16f126acc2feebb
opzmetek <op.zmetek@gmail.com>
2025-12-17 17:31:43 +0100

    Update client.js

--- client.js
+++ client.js
@@ -1,4 +1,5 @@
 import * as T2 from "https://dyt.neocities.org/t2/t2.module.js";
+alert(T2);
 const r=new T2.Renderer(document.getElementById("game"),new OffscreenCanvas(1600,800));
 await r.start();
 const ship=new T2.ORectangle(-0.5,-1,1,2);
