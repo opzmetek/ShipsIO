1ffdba90a7f4420a1161132ee0a6e6af575f7cae
opzmetek <op.zmetek@gmail.com>
2025-12-17 17:37:57 +0100

    Update client.js

--- client.js
+++ client.js
@@ -1,5 +1,6 @@
+console.log("t2 loading");
 import * as T2 from "https://dyt.neocities.org/t2/t2.module.js";
-alert(T2);
+console.log("t2 loaded");
 const r=new T2.Renderer(document.getElementById("game"),new OffscreenCanvas(1600,800));
 await r.start();
 const ship=new T2.ORectangle(-0.5,-1,1,2);
