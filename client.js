0adef96216edb01f4322760c811796e1e5103eed
opzmetek <op.zmetek@gmail.com>
2025-12-17 18:08:47 +0100

    Update image source for ship rendering

--- client.js
+++ client.js
@@ -2,7 +2,7 @@ console.log("t2 loading");
 import * as T2 from "https://shipsio.pages.dev/t2.module.js";
 console.log("t2 loaded");
 const img = new Image();
-img.src="https://www.reddit.com/r/battlemaps/comments/ls5z3m/transparent_ships/";//debug
+img.src="https://shipsio.pages.dev/image/ships.png";
 img.crossOrigin="anonymous";
 const r=new T2.Renderer(document.getElementById("game"),img);
 await r.start();
