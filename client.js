3145291fbea721568f2d5674982d5ed672fa32af
opzmetek <op.zmetek@gmail.com>
2025-12-26 10:37:20 +0100

    Refactor ship initialization and add movement controls
    Updated the ship's initial position and added key event listeners for movement controls.

--- client.js
+++ client.js
@@ -5,17 +5,40 @@ const img = new Image();
 img.src="https://shipsio.pages.dev/ships.jpg";
 img.crossOrigin="anonymous";
 await img.decode();
-const r=new T2.Renderer(document.getElementById("game"),img);
+const canvas = document.getElementById("game");
+const r=new T2.Renderer(canvas,img);
 await r.start();
 const ship=new T2.ORectangle(-3,-4,6,8);
 const world = r.world;
 const andromeda = new T2.Andromeda(r);
 const server = new WebSocket("wss://shipsioserver.onrender.com");
 await new Promise((r,e)=>{server.onopen=r;server.onerror=e;});
+server.onmessage = m=>console.log(m);
 start();
+const keys = {};
+canvas.addEventListener("keydown",e=>{
+  const k = e.keyCode;
+  switch(k){
+    case "w":
+      send({type:"move",vx:1});
+      break;
+    case "s":
+      send({type:"move",vx:-1});
+      break;
+    case "a":
+      send({type:"move",vy:1});
+      break;
+    case "d":
+      send({type:"move",vy:1});
+      break;
+  }
+});
+canvas.addEventListener("keyup",e=>{
+  delete keys[e.keyCode];
+});
 function start(){
   send({type:"init",name:"OPZ"});
-  world.add(ship,new V2(0,0),new V2(1,1));
+  world.add(ship,new V2(0,0),new V2(0.1,0.2));
 }
 function send(o){
   server.send(JSON.stringify(o));
