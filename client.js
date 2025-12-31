e0ee013bbfd29c2bf0a2915372d23b9ff9743c36
opzmetek <op.zmetek@gmail.com>
2025-12-31 17:43:04 +0100

    Update client.js

--- client.js
+++ client.js
@@ -9,7 +9,8 @@ await img.decode();
 const canvas = document.getElementById("game");
 canvas.onresize=resize;
 const r=new T2.Renderer(canvas,img);
-const ships = T2.ORectangle.instanced();
+const playersArray = [];
+const players = new Map();
 await r.start();
 const ship=new T2.ORectangle(-3,-4,6,8);
 const world = r.world;
@@ -18,6 +19,12 @@ const server = new WebSocket("wss://shipsioserver.onrender.com");
 await new Promise((r,e)=>{server.onopen=r;server.onerror=e;});
 document.getElementById("loader").style.display="none";
 server.onmessage = handle;
+server.onclose = e=>{
+	document.body.textContent = "Connection "+(e.wasClean?"closed by server":"interrupted")+" with error: "+e.code+": "+e.reason+".";
+}
+server.onerror = e=>{
+	document.body.textContent = "Connection interrupted by internal error.";
+}
 resize();
 start();
 const accumulator = new V2();
@@ -89,6 +96,35 @@ if("ontouchstart" in window||navigator.maxTouchPoints>0){
     else lId=null;
   });
 }
+
+class Player{
+	constructor(arr){
+		const [id,name,x,y,ship] = arr;
+		this.id = id;
+		this.name = name;
+		this.c = new V2(x,y);
+		this.vector = new V2(0,0);
+		this.angle = 0;
+		this.ship = ship;
+		this.hpUV = 1;
+		players.set(id,this);
+		playersArray.push(this);
+	}
+
+	move(arr){
+		const [id,x,y,vx,vy,angle] = arr;
+		this.angle = angle;
+		const v = new V2(vx,vy);
+		const p = new V2(x,y);
+		const d = p.subImm(this.c);
+		const l = d.sq();
+		if(l>=1) { //BIG DIFF - NO SMOOTHMOVE
+			this.c.add(d.scale(0.2));
+		}
+		this.vector.copy(vel);
+	}
+}
+
 function start(){
   send({type:"init",name:"OPZ"});
   world.add(ship,new V2(0,0),new V2(0.1,0.2));
@@ -111,7 +147,16 @@ async function handle(e){
 
 async function handleInit(d){
 	const data = JSON.parse(d.toString());
-	console.log("Your ID is: "+data.id);
+	switch(data.type){
+		case "init":
+			console.log("Your ID is: "+data.id);
+			data.setup.forEach(p=>new Player(p));
+			break;
+		case "connect":
+			console.log("Connected new Player: "+data.player[1]);
+			new Player(data.player);
+			break;
+	}
 }
 
 async function handlePlayers(d){
